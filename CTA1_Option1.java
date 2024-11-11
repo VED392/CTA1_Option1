9213f92a2b346fec6c17ae0a07e81ae91431abc8
VED392 <jorge.perez@csuglobal.edu>
2024-11-11 07:14:22 -0800

    first commit

---
+++ CTA1_Option1.java
@@ -0,0 +1,18 @@
+public class CTA1_Option1{
+    public static void main(String[] args){
+
+        String first_name = "John";
+        String last_name = "NhoJ";
+        String street_address = "232 mele st  Ca 9000001 ";
+        String city = "Los angles";
+        String zipcode = "900001";
+        
+        System.out.println(first_name);
+        System.out.println(last_name);
+        System.out.println(street_address);
+        System.out.println(city);
+        System.out.println(zipcode);
+
+    }
+}
+
